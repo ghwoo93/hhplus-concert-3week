07ceb69a861aa90b91e5149669abbc581ef72e9d
ghwoo-rbrain <ghwoo@rbrain.co.kr>
2024-07-05 09:56:31 +0900

    - dto 생성자 추가

--- src/main/java/io/hhplus/concert/reservation/aggregate/dto/ReservationSeat.java
+++ src/main/java/io/hhplus/concert/reservation/aggregate/dto/ReservationSeat.java
@@ -2,9 +2,11 @@ package io.hhplus.concert.reservation.aggregate.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReservationSeat {
     private int seatNumber;
     private boolean available;
