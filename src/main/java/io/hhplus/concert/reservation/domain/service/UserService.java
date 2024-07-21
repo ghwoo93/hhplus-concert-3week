ffc2d6a0a9d5e016a8bb7a378d815ef4cc70d428
BOOK-BBIGU\Administrator <wkungh93@gmail.com>
2024-07-21 21:45:16 +0900

    feat: 사용자 중복검사 기능 추가

--- src/main/java/io/hhplus/concert/reservation/domain/service/UserService.java
+++ src/main/java/io/hhplus/concert/reservation/domain/service/UserService.java
@@ -6,8 +6,9 @@ import io.hhplus.concert.reservation.domain.model.User;
 import io.hhplus.concert.reservation.presentation.response.BalanceResponse;
 
 public interface UserService {
+    boolean existsById(String userId);
     BalanceResponse rechargeBalance(String userId, BigDecimal amount);
     BalanceResponse getBalance(String userId);
     User getUser(String userId);
-    BalanceResponse deductBalance(String userId, BigDecimal amount);    
+    BalanceResponse deductBalance(String userId, BigDecimal amount);
 }
--- src/main/java/io/hhplus/concert/reservation/infrastructure/entity/UserEntity.java
+++ src/main/java/io/hhplus/concert/reservation/infrastructure/entity/UserEntity.java
@@ -4,12 +4,12 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import io.hhplus.concert.reservation.domain.model.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -23,6 +23,8 @@ public class UserEntity {
     private String username;
     private String password;
     private BigDecimal balance;
+    
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     public UserEntity(User user) {
